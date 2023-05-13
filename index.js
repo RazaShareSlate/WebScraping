c57c114943f14908746e4422e197c1c9012a6ffa
M Raza <116066466+RazaShareSlate@users.noreply.github.com>
2023-05-14 01:37:19 +0500

    take screenshot and upload on gyazo

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const fs = require('fs');
+const Gyazo = require('gyazo-api');
 
 let chrome = {};
 let puppeteer;
@@ -7,11 +9,14 @@ const app = express();
 app.use(express.json());
 
 
+const GYAZA_ACCESS_TOKEN = '7PzEIcS3B2pRB1sMOOjtzBrGR4PX04ZH0ZfMc9bxRmk'
+var gyazoClient = new Gyazo(GYAZA_ACCESS_TOKEN);
+
 if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
-  chrome = require("chrome-aws-lambda");
-  puppeteer = require("puppeteer-core");
+     chrome = require("chrome-aws-lambda");
+     puppeteer = require("puppeteer-core");
 } else {
-  puppeteer = require("puppeteer");
+     puppeteer = require("puppeteer");
 }
 
 app.get("/", async (req, res) => {
@@ -21,11 +26,11 @@ app.get("/", async (req, res) => {
 const isValidUrl = (urlString) => {
      var urlPattern = new RegExp(
           "^(https?:\\/\\/)?" + // validate protocol
-               "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
-               "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
-               "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
-               "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
-               "(\\#[-a-z\\d_]*)?$",
+          "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // validate domain name
+          "((\\d{1,3}\\.){3}\\d{1,3}))" + // validate OR ip (v4) address
+          "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // validate port and path
+          "(\\?[;&a-z\\d%_.~+=-]*)?" + // validate query string
+          "(\\#[-a-z\\d_]*)?$",
           "i"
      ); // validate fragment locator
      return !!urlPattern.test(urlString);
@@ -39,13 +44,13 @@ app.post("/", async (req, res) => {
 
      if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
           options = {
-            args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
-            defaultViewport: chrome.defaultViewport,
-            executablePath: await chrome.executablePath,
-            headless: true,
-            ignoreHTTPSErrors: true,
+               args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+               defaultViewport: chrome.defaultViewport,
+               executablePath: await chrome.executablePath,
+               headless: true,
+               ignoreHTTPSErrors: true,
           };
-        }
+     }
 
      if (!url) {
           return res.send("please enter website Url").status("404");
@@ -60,15 +65,67 @@ app.post("/", async (req, res) => {
           const page = await browser.newPage();
           await page.goto(url);
           const content = await page.evaluate(() => document.body.innerText);
-          console.log({content});
-          if(content){
+          console.log({ content });
+          if (content) {
 
-     
+
           }
           await browser.close();
-          res.send({content:`${content}`} ).status("200");
+          res.send({ content: `${content}` }).status("200");
      } catch (error) {
-          res.send( error ).status(404);
+          res.send(error).status(404);
+     }
+});
+
+
+app.post("/screenshot", async (req, res) => {
+     let options = {};
+     const url = req?.body?.url;
+
+
+     if (process.env.AWS_LAMBDA_FUNCTION_VERSION) {
+          options = {
+               args: [...chrome.args, "--hide-scrollbars", "--disable-web-security"],
+               defaultViewport: chrome.defaultViewport,
+               executablePath: await chrome.executablePath,
+               headless: true,
+               ignoreHTTPSErrors: true,
+          };
+     }
+
+     if (!url) {
+          return res.send("please enter website Url").status("404");
+     }
+     if (!isValidUrl(url)) {
+          return res.send("URL is not valid").status("400");
+     }
+
+     try {
+          let browser = await puppeteer.launch(options);
+
+          const page = await browser.newPage();
+          await page.goto(url, { waitUntil: "networkidle0",timeout:100000 });
+          // await page.waitForLoadState('networkidle');
+          const screenshot = await page.screenshot({  path: "page.png", fullPage: true, type: "png", encoding: 'binary' });
+
+          // const imageData = fs.readFileSync('./page.png');
+
+
+          const gyazoRes = await gyazoClient.upload('./page.png');
+          // console.log(gyazoRes?.data?);
+          await browser.close();
+          res.send({
+               mesaage: "image upload successfully",
+               imageUrl: gyazoRes?.data?.url,
+               data: gyazoRes?.data
+          }).status("200");
+
+     } catch (error) {
+          console.log(error);
+          res.send({
+               mesaage: "Internal Server Error",
+               error: error
+          }).status(500);
      }
 });
 
